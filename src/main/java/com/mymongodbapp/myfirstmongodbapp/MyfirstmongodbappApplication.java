538b4b7f20ad5a8a5dadd0618dbb202f82ea79f9
Swathi Pamarthi <swathi.kasagani@gmail.com>
2020-03-27 18:27:39 -0500

    my first commit

---
+++ src/main/java/com/mymongodbapp/myfirstmongodbapp/MyfirstmongodbappApplication.java
@@ -0,0 +1,14 @@
+package com.mymongodbapp.myfirstmongodbapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstmongodbappApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MyfirstmongodbappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mymongodbapp/myfirstmongodbapp/MyfirstmongodbappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mymongodbapp.myfirstmongodbapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirstmongodbappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
